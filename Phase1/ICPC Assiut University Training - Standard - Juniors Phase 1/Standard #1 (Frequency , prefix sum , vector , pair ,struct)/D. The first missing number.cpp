892fe04e729f42874241b950dd81cdc12071a4ea
mohammedrateb <se.mohamedrateb@gmail.com>
2026-05-14 04:22:45 +0200

    Solve :  D. The first missing number.cpp

---
+++ Phase1/ICPC Assiut University Training - Standard - Juniors Phase 1/Standard #1 (Frequency , prefix sum , vector , pair ,struct)/D. The first missing number.cpp
@@ -0,0 +1,149 @@
+/*
+ *  قَالَ رَبِّ اشْرَحْ لِي صَدْرِي وَيَسِّرْ لِي أَمْرِي وَاحْلُلْ عُقْدَةً مِّن لِّسَانِي يَفْقَهُوا قَوْلِي *
+ ▄▄       ▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄       ▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄
+▐░░▌     ▐░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░▌     ▐░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░▌
+▐░▌░▌   ▐░▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌░▌   ▐░▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌
+▐░▌▐░▌ ▐░▌▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌▐░▌ ▐░▌▐░▌▐░▌          ▐░▌       ▐░▌
+▐░▌ ▐░▐░▌ ▐░▌▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌ ▐░▐░▌ ▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌       ▐░▌
+▐░▌  ▐░▌  ▐░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌  ▐░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌
+▐░▌   ▀   ▐░▌▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌   ▀   ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░▌       ▐░▌
+▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌          ▐░▌       ▐░▌
+▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌
+▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░▌
+ ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀         ▀  ▀         ▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀
+
+ ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░▌
+▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌ ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌
+▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌     ▐░▌          ▐░▌       ▐░▌
+▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌     ▐░▌     ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌
+▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░▌
+▐░█▀▀▀▀█░█▀▀ ▐░█▀▀▀▀▀▀▀█░▌     ▐░▌     ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌
+▐░▌     ▐░▌  ▐░▌       ▐░▌     ▐░▌     ▐░▌          ▐░▌       ▐░▌
+▐░▌      ▐░▌ ▐░▌       ▐░▌     ▐░▌     ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌
+▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░▌
+ ▀         ▀  ▀         ▀       ▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀
+ */
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+#define pi 3.141592
+#define ull unsigned long long int
+#define ll long long
+#define ld long double
+#define endl '\n'
+#define OUT cout
+#define IN cin
+#define FC                            \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+class func
+{
+public:
+    ull nCr(int n, int r)
+    {
+        if (r > n)
+            return 0;
+        r = max(r, n - r);
+        unsigned long long ans = 1, div = 1, i = r + 1;
+        while (i <= n)
+        {
+            ans *= i;
+            i++;
+            ans /= div;
+            div++;
+        }
+        return ans;
+    }
+
+    ull nPr(int n, int r)
+    {
+        if (r > n)
+            return 0;
+        unsigned long long p = 1, i = n - r + 1;
+        while (i <= n)
+            p *= i++;
+        return p;
+    }
+    ull fact(ull num)
+    {
+        if (num == 0 || num == 1)
+            return 1;
+        return num * fact(num - 1);
+    }
+    ull gcd(ull a, ull b)
+    {
+        if (b == 0)
+        {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+    ull lcm(ull a, ull b)
+    {
+        return a / gcd(a, b) * b;
+    }
+    bool isprime(ll num)
+    {
+        if (num == 1)
+            return false;
+        // if (num == 2 || num == 3) return true;
+        // if (num % 2 == 0 || num % 3 == 0)return false;
+        for (int i = 2; i * i <= num; i++)
+        {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+};
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> arr(n);
+    vector<int> freq1(m + 1);
+    vector<int> freq2(m + 1);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        if (arr[i] < 0)
+            freq1[abs(arr[i])]++;
+        else
+            freq2[arr[i]]++;
+    }
+    for (int i = -m; i <= m; i++)
+    {
+        if (i < 0)
+        {
+            if (freq1[abs(i)] == 0)
+            {
+                cout << i << endl;
+                return;
+            }
+        }
+        else
+        {
+            if (freq2[abs(i)] == 0)
+            {
+                cout << i << endl;
+                return;
+            }
+        }
+    }
+}
+int main()
+{
+    FC // Fast Code
+       // Test Git
+       // Edited On Git
+        int t = 1;
+    // cin >> t;
+    while (t--)
+        solve();
+    // cout << fixed << setprecision(9);
+}
